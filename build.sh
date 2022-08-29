0d524deca7f0e4b655938fa487f535eee48cf42a
ablewitt <4407104+ablewitt@users.noreply.github.com>
2022-08-29 22:20:47 +1000

    Move to swagger

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Download openapi spec
-curl https://input-output-hk.github.io/cardano-wallet/api/edge/swagger.yaml -o swagger.yaml
-
-# Generate
-openapi-generator generate -i swagger.yaml -g java -c cardano-wallet-api-config.json -o output
+# Download and generate
+swagger-codegen generate -i https://input-output-hk.github.io/cardano-wallet/api/edge/swagger.yaml \
+-l java -o output -c cardano-wallet-api-config.json
